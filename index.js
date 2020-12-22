ac6899c8a5a09e34c020f69e7504603cedb00511
Marcin Gromek <marcingromek101@gmail.com>
2020-12-22 16:07:24 +0100

    getting started with rest api

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+
+let pieRepo = require("./pieRepo");
+let router = express.Router();
+
+router.get("/", function (req, res, next) {
+  pieRepo.get(
+    function (data) {
+      res.status(200).json({
+        status: 200,
+        statusText: "OK",
+        message: "all pies retrieved",
+        data: data,
+      });
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+router.get("/:id", function (req, res, next) {
+  pieRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "OK",
+          message: "all pies retrieved",
+          data: data,
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: "not found",
+          message: `the pie ${req.params.id} is not found`,
+          error: {
+            code: "NOT_FOUND",
+            message: `the pie ${req.params.id} is not found`,
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+router.get("/search", function (req, res, next) {
+  let searchObject = {
+    id: req.query.id,
+    name: req.query.name,
+  };
+
+  pieRepo.search(
+    searchObject,
+    function (data) {
+      if (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "OK",
+          message: "all pies retrieved",
+          data: data,
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: "not found",
+          message: `the pie ${req.params.id} is not found`,
+          error: {
+            code: "NOT_FOUND",
+            message: `the pie ${req.params.id} is not found`,
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+app.use("/api/", router);
+
+var server = app.listen(5000, function () {
+  console.log("Node server is running on http://localhost:5000...");
+});
